87584e44c81ad74ec935929e6f125663c3e49bbe
Nayak, Neha Maruti <nmnayak@iu.edu>
2020-02-10 07:35:35 -0500

    Storing image to database

---
+++ demo/src/main/java/com/example/demo/controllers/KafkaController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.engine.Producer;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+	private final Producer producer;
+
+    @Autowired
+    KafkaController(Producer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping(value = "/publish")
+    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+        this.producer.sendMessage(message);
+    }
+}
---
+++ demo/src/main/java/com/example/demo/models/ReceivedData.java
@@ -0,0 +1,7 @@
+package com.example.demo.models;
+
+public class ReceivedData {
+	String resultPlot;
+	String uid;
+	String userId;
+}
